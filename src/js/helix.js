dc698ebc4d5c63cc56dd27e8f1502405eedbc2d7
Greg <ggydush@broadinstitute.org>
2020-01-31 00:04:55 -0500

    Clean up

--- src/js/helix.js
+++ src/js/helix.js
@@ -1,9 +1,19 @@
-var scene = new THREE.Scene();
-// scene.background = new THREE.Color( '#FFFFFF' );
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100 );
-var loader = new THREE.GLTFLoader();
+
+// Create new scene
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+let loader = new THREE.GLTFLoader();
+let container = document.getElementById('canvas');
+let renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+container.appendChild(renderer. domElement);
 var model
 
+// Adjust camera position
+camera.position.z = 3;
+camera.position.x = -3
+
+// Load 3D model
 loader.load('./src/assets/helix.gltf', function (gltf) {
     scene.add(gltf.scene);
     model = gltf.scene;
@@ -11,25 +21,12 @@ loader.load('./src/assets/helix.gltf', function (gltf) {
     console.error(error);
 } );
 
-
-container = document.getElementById('canvas');
-// document.body.appendChild(container);
-renderer = new THREE.WebGLRenderer( { alpha: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setSize(window.innerWidth, window.innerHeight);
-container.appendChild( renderer.domElement );
-renderer.domElement.style = 'z-index: 0;'
-
-
-camera.position.z = 3;
-camera.position.x = -3
+// Render the model on animation loop
 function animate() {
     requestAnimationFrame( animate );
-    // model.rotation.y += 0.05
     renderer.render( scene, camera );
 }
 animate();
-
 
 // Handle window resize
 window.addEventListener( 'resize', onWindowResize, false );
@@ -38,14 +35,13 @@ window.addEventListener( 'resize', onWindowResize, false );
         camera.updateProjectionMatrix();
         renderer.setSize( window.innerWidth, window.innerHeight );
         camera.position.x = -1 - window.innerWidth / 1000
-
 }
 
-var rotation = 0,
-scrollLoc = $('.container').scrollTop();
+// Handle rotation
+loc = $('.container').scrollTop();
 $('.container').scroll(function() {
     var newLoc = $('.container').scrollTop();
-    var diff = scrollLoc - newLoc;
-    scrollLoc = newLoc;
+    var diff = loc - newLoc;
+    loc = newLoc;
     model.rotation.y += diff / 200
 });
