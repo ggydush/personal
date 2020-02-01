ba934d65c9cfe31d77abeb8b6123168f56a29263
Greg <ggydush@broadinstitute.org>
2020-02-01 14:35:17 -0500

    Adding lighting/color to model and added skills

--- src/js/helix.js
+++ src/js/helix.js
@@ -1,20 +1,23 @@
 
 // Create new scene
+let container = document.getElementById('model');
+const width = container.offsetWidth
+const height = container.offsetHeight
 let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+let camera = new THREE.PerspectiveCamera(75, width / height, 1, 100);
 let loader = new THREE.GLTFLoader();
-let container = document.getElementById('canvas');
-let renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-container.appendChild(renderer. domElement);
+let renderer = new THREE.WebGLRenderer({ alpha: true , antialias: true });
+let light = new THREE.PointLight( 0x44a08d , 3, 100 );
+light.position.set(0, 50, 0);
+scene.add(light);
+camera.position.z = 3;
+renderer.setSize(width, height);
+container.appendChild(renderer.domElement);
 var model
 
-// Adjust camera position
-camera.position.z = 3;
-camera.position.x = -3
 
-// Load 3D model
-loader.load('./src/assets/helix.gltf', function (gltf) {
+// Load model
+loader.load('./src/assets/old/helix.gltf', function (gltf) {
     scene.add(gltf.scene);
     model = gltf.scene;
 }, undefined, function (error) {
@@ -28,14 +31,6 @@ function animate() {
 }
 animate();
 
-// Handle window resize
-window.addEventListener( 'resize', onWindowResize, false );
-    function onWindowResize() {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        camera.position.x = -1 - window.innerWidth / 1000
-}
 
 // Handle rotation
 loc = $('.container').scrollTop();
@@ -45,3 +40,18 @@ $('.container').scroll(function() {
     loc = newLoc;
     model.rotation.y += diff / 200
 });
+
+$(function() {
+    $('.pylibs').hover(function() {
+      $('.pylibs').css('color', 'rgba(255, 255, 255, 0.468)');
+    }, function() {
+      $('.pylibs').css('color', 'black');
+    });
+  });
+  $(function() {
+    $('.jslibs').hover(function() {
+      $('.jslibs').css('color', 'rgba(255, 255, 255, 0.468)');
+    }, function() {
+      $('.jslibs').css('color', 'black');
+    });
+  });
